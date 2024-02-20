a9d205e04574dc16f73476f71d824b17b7ed8faa
Vlaimir Korba <krissky1986@gmail.com>
2024-02-20 22:20:30 +0200

    71 RxJS of()

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -15,33 +15,38 @@ export class AppComponent {
 
   data: any[] = [];
 
+  array1 = [1, 3, 5, 7, 9];
+  array2 = ['A', 'B', 'C', 'D'];
+
   //1. CREATE AN OBSERVABLE
   // Observable
-  myObservable = new Observable((observer) => {
-    setTimeout(() => {
-      observer.next(1);
-    }, 1000);
-    setTimeout(() => {
-      observer.next(2);
-    }, 2000);
-    setTimeout(() => {
-      observer.next(3);
-    }, 3000);
-    // setTimeout(() => {
-    //   observer.error(
-    //     new Error('Something went wrong. Please try again later.')
-    //   );
-    // }, 3000);
-    setTimeout(() => {
-      observer.next(4);
-    }, 4000);
-    setTimeout(() => {
-      observer.next(5);
-    }, 5000);
-    setTimeout(() => {
-      observer.complete();
-    }, 6000);
-  });
+  // myObservable = new Observable((observer) => {
+  //   setTimeout(() => {
+  //     observer.next(1);
+  //   }, 1000);
+  //   setTimeout(() => {
+  //     observer.next(2);
+  //   }, 2000);
+  //   setTimeout(() => {
+  //     observer.next(3);
+  //   }, 3000);
+  //   // setTimeout(() => {
+  //   //   observer.error(
+  //   //     new Error('Something went wrong. Please try again later.')
+  //   //   );
+  //   // }, 3000);
+  //   setTimeout(() => {
+  //     observer.next(4);
+  //   }, 4000);
+  //   setTimeout(() => {
+  //     observer.next(5);
+  //   }, 5000);
+  //   setTimeout(() => {
+  //     observer.complete();
+  //   }, 6000);
+  // });
+
+  myObservable = of(this.array1, this.array2, 20, 30, 'Hello', true);
 
   // Observer
   // next, error, complete
@@ -60,6 +65,7 @@ export class AppComponent {
     this.myObservable.subscribe({
       next: (val: any) => {
         this.data.push(val);
+        console.log(val);
       },
       error(err) {
         alert(err.message);
