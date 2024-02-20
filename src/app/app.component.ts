16d89a77dd4e8b0662d1f3a684829ba2339aa0e0
Vlaimir Korba <krissky1986@gmail.com>
2024-02-20 21:09:41 +0200

    #69 Creating & Using an Observable | Understanding Observables & RxJS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-observables';
+
+  data: any[] = [];
+
+  //1. CREATE AN OBSERVABLE
+  // Observable
+  myObservable = new Observable((observer) => {
+    setTimeout(() => {
+      observer.next(1);
+    }, 1000);
+    setTimeout(() => {
+      observer.next(2);
+    }, 2000);
+    setTimeout(() => {
+      observer.next(3);
+    }, 3000);
+    setTimeout(() => {
+      observer.next(4);
+    }, 4000);
+    setTimeout(() => {
+      observer.next(5);
+    }, 5000);
+  });
+
+  // Observer
+  // next, error, complete
+  GetAsyncData() {
+    this.myObservable.subscribe((val: any) => {
+      this.data.push(val);
+    });
+  }
 }
