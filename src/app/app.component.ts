d6b1681be35705ec0c95e18dd47c09646f652122
Vlaimir Korba <krissky1986@gmail.com>
2024-02-22 13:30:41 +0200

    #72 The fromEvent Operator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { resolve } from 'node:path';
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { Observable, of, from } from 'rxjs';
+import { Observable, of, from, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -11,78 +11,35 @@ import { Observable, of, from } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'angular-observables';
 
-  data: any[] = [];
+  @ViewChild('createbtn')
+  createBtn: ElementRef;
 
-  array1 = [1, 3, 5, 7, 9];
-  array2 = ['A', 'B', 'C', 'D'];
+  createBtnObs;
 
-  //1. CREATE AN OBSERVABLE
-  // Observable
-  // myObservable = new Observable((observer) => {
-  //   setTimeout(() => {
-  //     observer.next(1);
-  //   }, 1000);
-  //   setTimeout(() => {
-  //     observer.next(2);
-  //   }, 2000);
-  //   setTimeout(() => {
-  //     observer.next(3);
-  //   }, 3000);
-  //   // setTimeout(() => {
-  //   //   observer.error(
-  //   //     new Error('Something went wrong. Please try again later.')
-  //   //   );
-  //   // }, 3000);
-  //   setTimeout(() => {
-  //     observer.next(4);
-  //   }, 4000);
-  //   setTimeout(() => {
-  //     observer.next(5);
-  //   }, 5000);
-  //   setTimeout(() => {
-  //     observer.complete();
-  //   }, 6000);
-  // });
-  // The of operator creates an observable from the arguments that we pass into it.You can pass any number of arguments to of operator
-  // Earch argument is emitted separately one after the other.It send the complete signal at the end.
-  // myObservable = of(this.array1, this.array2, 20, 30, 'Hello', true);
-
-  promiseData = new Promise((resolve, reject) => {
-    resolve([10, 20, 30, 40, 50]);
-  });
-
-  // The from operator takes a single argument which can be iterated over and convers it into an observable
-  myObservable = from(this.promiseData);
-  // myObservable = from(this.array1);
-
-  // Observer
-  // next, error, complete
-  GetAsyncData() {
-    // this.myObservable.subscribe(
-    //   (val: any) => {
-    //     this.data.push(val);
-    //   },
-    //   (err) => {
-    //     alert(err.message);
-    //   },
-    //   () => {
-    //     alert('All the data streamed!');
-    //   }
-    // );
-    this.myObservable.subscribe({
-      next: (val: any) => {
-        this.data.push(val);
-        console.log(val);
-      },
-      error(err) {
-        alert(err.message);
-      },
-      complete() {
-        alert('All the data streamed!');
-      },
+  buttonClicked() {
+    let count = 0;
+    // will be returned as observable
+    // using fromEvent we created observable from the 'click event'
+    this.createBtnObs = fromEvent(
+      this.createBtn.nativeElement,
+      'click'
+    ).subscribe((data) => {
+      console.log(data);
+      this.showItem(++count);
     });
+  }
+
+  ngAfterViewInit() {
+    this.buttonClicked();
+  }
+
+  showItem(val) {
+    let div = document.createElement('div');
+    div.innerText = 'Item' + val;
+    div.className = 'data-list';
+    document.getElementById('container').appendChild(div);
   }
 }
